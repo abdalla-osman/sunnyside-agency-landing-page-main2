7367b7dafc819ba417cdde20236b4b24c2432ba7
abdalla-osman <abdallapython6000@gmail.com>
2024-09-13 08:17:48 -0700

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,6 @@
+let navbarToggler=document.querySelector('.navbar-toggler');
+let  navbar=document.querySelector('.navbar-mboile');
+navbarToggler.addEventListener('click',()=>{
+   navbar.classList.toggle('active')
+
+})
